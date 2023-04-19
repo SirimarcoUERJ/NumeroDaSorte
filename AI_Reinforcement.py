50c2a28ae83d90c25697cb3d34bfb8c159f65930
--global <--global>
2023-04-19 00:43:08 -0300

    Salva a tabela sem precisar passar o nome

--- AI_Reinforcement.py
+++ AI_Reinforcement.py
@@ -6,31 +6,32 @@ import time
 
 class Q_Ai:
     
-    def __init__(self, line_number, filename, exploitation=0.95, learn_rate=0.01, discount=0.9):
+    def __init__(self, line_number, exploitation=0.95, learn_rate=0.01, discount=0.9):
         """Inicializa a classe Q_Ai."""
         self.line_number = line_number + 1 # numero + 1 para adicionar a possibilidade do 0
         self.column_number = 2 * self.line_number  # o numero de linhas vezes dois pois cada um tem a possibilidade de maior ou menor
         self.exploitation = exploitation
         self.learn_rate = learn_rate
         self.discount = discount
-        self.create_or_load_table(filename)
+        self.create_or_load_table(line_number)
         self.guide = self.create_vector_array(self.line_number)
         
-    def save_table(self, filename):
-        """Salva a tabela Q em um arquivo com o nome 'filename'"""
-        with open(filename, "wb") as f:
-            pkl.dump(self.Q_table, f)
-    
-    def create_or_load_table(self, filename):
+    def create_or_load_table(self, possibility_number):
         """Tenta abrir o arquivo com o nome 'filename' contendo a tabela Q, 
         se o arquivo não existir, cria uma nova tabela Q e a salva no arquivo."""
         try:
-            with open(filename, "rb") as f:
+            self.filename = "Luck_game_agent["+str(possibility_number)+"].pkl"
+            with open(self.filename, "rb") as f:
                 self.Q_table = pkl.load(f)
         except FileNotFoundError:
             # self.Q_table = np.random.randn(self.line_number, self.column_number)
             self.Q_table = np.zeros((self.line_number, self.column_number))
-            self.save_table(filename)
+            self.save_table()
+    
+    def save_table(self):
+        """Salva a tabela Q em um arquivo com o nome 'filename'"""
+        with open(self.filename, "wb") as f:
+            pkl.dump(self.Q_table, f)
     
     def create_vector_array(self, n):
         """Cria um vetor de tamanho 'n', em que cada elemento é um vetor contendo 
@@ -97,10 +98,10 @@ class Q_Ai:
         # Equação de reforço para atualizar o valor da tabela Q
         self.Q_table[line][column] = (1 - self.learn_rate) * self.Q_table[line][column] + self.learn_rate * (reward + (1 - self.discount) * 15)
 
-        if self.Q_table[line][column] > 5:
-            self.Q_table[line][column] = 5
-        if self.Q_table[line][column] < -5:
-            self.Q_table[line][column] = -5
+        if self.Q_table[line][column] > 3:
+            self.Q_table[line][column] = 3
+        if self.Q_table[line][column] < -3:
+            self.Q_table[line][column] = -3
 
         return None
 
@@ -143,7 +144,7 @@ class Q_Ai:
 
         # Recebe a matriz de dados
         dataL, dataG = self.separate_matrix(self.Q_table)
-        arr = np.arange(0, self.line_number, 1)
+        arr = np.arange(0, self.line_number, int(self.line_number/10))
 
         # Crie o gráfico de calor usando imshow
         fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(6,10))
