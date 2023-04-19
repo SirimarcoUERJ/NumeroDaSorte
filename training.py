81e670840b3203a82ef618fcbc9c604425098c54
--global <--global>
2023-04-19 00:44:54 -0300

    Tratamento de erro e barra de progresso do treinamento

--- training.py
+++ training.py
@@ -1,51 +1,94 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import AI_Reinforcement as ML
 import Lucky_game as BS
+import time
 
-# Define o número máximo possível e o nome do arquivo que irá armazenar a tabela Q
-possiveis_numeros_da_sorte = 10
-filename = "Luck_game.pkl"
+def progress_bar(total, atual, previous_percent, txt):
+    percent = round((atual/total)*100,2)
+    # print(previous_percent, percent)
+    if (percent - previous_percent) > 0.01 and percent <= 100:
+        bar = "["
+        for i in range(int(percent//5)):
+            bar += "#"
+        
+        for i in range(20-len(bar)):
+            bar += "_"
+        
+        bar += "]"
+
+        print("\033c", end="")
+        print("{}\n{} {}%".format(txt, bar, percent))
+    else:
+        percent = previous_percent
+
+    return percent
+
+while True:
+    try:
+        # Rodadas de Treinamentos
+        rodadas_treino = int(input("Quantas rodadas de treino vamos fazer?(int) "))
+        
+        # Define o número máximo possível da tabela Q
+        possiveis_numeros_da_sorte = int(input("\nEscolha o limite superior para treinarmos a 'AI'\nlimite inferior = 0.\nLimite Superior = "))
+        
+        break
+    except:
+        print("\nErro! Os valores esperados são do Tipo <class 'int'>.\n")
+
+total = possiveis_numeros_da_sorte * 1000
+atual = 0 
 
 # Cria instâncias do jogo e da IA
 game = BS.Game(maior_possivel=possiveis_numeros_da_sorte)
-AI = ML.Q_Ai(possiveis_numeros_da_sorte, filename, 0.01)
+AI = ML.Q_Ai(possiveis_numeros_da_sorte, 0.5)
 
-# Define as variáveis iniciais do jogo
-running = True
-partida = 1
-partidas_boas = 0
+for i in range(rodadas_treino):
+    percent = 0
+    txt = "{}° Rodada de Treino".format(i+1)
 
-while running:
-    if game.times == 0:
-        while True:
-            number = AI.choose_action()
-            if number != game.number:
-                break
-        feedback = game.Set_choice(number)
-        game.times -= 1
+    # Define as variáveis iniciais do jogo
+    running = True
+    partida = 1
+    partidas_boas = 0
+    try_map = np.array([])
+
+    while running:
+        
+        if game.times == 0:
+            while True:
+                number = AI.choose_action()
+                if number != game.number:
+                    break
+            feedback = game.Set_choice(number)
+            game.times -= 1
+            state = number, feedback
+
+        next_number = AI.choose_action(state)
+        next_feedback = game.Set_choice(next_number)
         state = number, feedback
+        reward = AI.calculate_reward(number, next_number, feedback)
 
-    next_number = AI.choose_action(state)
-    next_feedback = game.Set_choice(next_number)
-    state = number, feedback
-    reward = AI.calculate_reward(number, next_number, feedback)
+        if partidas_boas > possiveis_numeros_da_sorte*1000:
+            AI.save_table()
+            break
 
-    if partidas_boas > 1000:
-        AI.save_table(filename)
-        break
+        if game.won(next_number):
+            try_map = np.append(try_map, game.times)
+            AI.update_Q_table((number, feedback), next_number, AI.calculate_reward(number, next_number, next_feedback))
+            partida += 1
 
-    if game.won(next_number):
-        AI.update_Q_table((number, feedback), next_number, AI.calculate_reward(number, next_number, next_feedback))
-        # print("Partida n°{} teve {} tentativa para acertar o numero da sorte {}.".format(partida, game.times, game.number))
-        partida += 1
+            if game.times < possiveis_numeros_da_sorte*0.75:
+                partidas_boas += 1
 
-        if game.times < 10:
-            partidas_boas += 1
+            game.restart_game()
+        else:
+            AI.update_Q_table((number, feedback), next_number, reward)
 
-        game.restart_game()
-    else:
-        AI.update_Q_table((number, feedback), next_number, reward)
+        number = next_number
+        feedback = next_feedback
+        
+        percent = progress_bar(possiveis_numeros_da_sorte * 1000, partidas_boas, percent, txt)
 
-    number = next_number
-    feedback = next_feedback
-
+# Plot um grafico com a politica do agente.
 AI.heatmap()
